59834bc24326dcb5c2f89ad800c1293c06722a7e
alittlebird <1025210608@qq.com>
2020-09-25 09:34:56 +0800

    Update device_discover.py

--- device_discover.py
+++ device_discover.py
@@ -3,6 +3,18 @@ import sys
 ip = "239.255.255.250"
 port = 3702
 
+message = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" \
+         "<Envelope xmlns:dn=\"http://www.onvif.org/ver10/network/wsdl\" xmlns=\"http://www.w3.org/2003/05/soap-envelope\">" \
+         "<Header><wsa:MessageID xmlns:wsa=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\">" \
+         "uuid:fc0bad56-5f5a-47f3-8ae2-c94a4e907d70</wsa:MessageID>" \
+         "<wsa:To xmlns:wsa=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\">" \
+         "urn:schemas-xmlsoap-org:ws:2005:04:discovery</wsa:To>" \
+         "<wsa:Action xmlns:wsa=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\">" \
+         "http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</wsa:Action>" \
+         "</Header><Body><Probe xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" " \
+         "xmlns=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\">" \
+         "<Types>dn:NetworkVideoTransmitter</Types><Scopes /></Probe></Body></Envelope>"
+
 def getOnvifIp():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # udp
